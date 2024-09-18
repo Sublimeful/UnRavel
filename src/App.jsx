6d2be1a0301604524b978ccdb042b2bef8f1dd79
Sublimeful <Qiangwu2000@gmail.com>
2024-09-18 11:04:33 -0500

    Added express.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root"));
-
-root.render(<h1>Hello, world</h1>);
+export default function App() {
+  return <h1>Hello world!</h1>;
+}
