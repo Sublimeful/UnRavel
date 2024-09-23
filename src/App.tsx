724174a82c75e52ba6965e6207c0c197ef281152
Sublimeful <Qiangwu2000@gmail.com>
2024-09-22 21:58:10 -0500

    Added shaders to the background of the main menu, currently using test shaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
+import { useEffect, useRef } from "react";
+
 export default function App() {
+  const glslCanvas = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (!glslCanvas.current) return;
+    // Component is mounted, window is available
+    glslCanvas.current.width = window.innerWidth;
+    glslCanvas.current.height = window.innerHeight;
+  }, [glslCanvas.current]);
+
   return (
     <div className="h-screen w-screen bg-[#040039] flex items-center justify-center">
-      <div className="absolute h-full w-full grid grid-rows-10 grid-cols-10 blur-2xl overflow-clip">
-        {/* RED */}
-        <div className="row-start-2 col-start-2 row-span-3 col-span-2 bg-[#E24F3B] rounded-full h-full aspect-square place-self-center" />
-        {/* CYAN */}
-        <div className="row-start-1 col-start-7 row-span-5 col-span-4 bg-[#4DDBDB] rounded-full h-full aspect-square place-self-center" />
-        {/* BLUE */}
-        <div className="row-start-7 col-start-3 row-span-4 col-span-3 bg-[#4754E5] rounded-full h-full aspect-square place-self-center" />
-      </div>
+      <canvas
+        ref={glslCanvas}
+        className="glslCanvas w-full h-full"
+        data-fragment-url="/shader.frag?raw"
+        data-textures="/moon.jpg"
+      ></canvas>
       <div className="absolute h-5/6 w-3/4 max-w-xl bg-[#000625] bg-opacity-50 rounded-xl border border-white flex flex-col items-center justify-center text-white">
         <img src="/logo.png" className="w-40 aspect-square" />
         <h1 className="text-[#DB1F3C] text-5xl font-bold">UnRavel</h1>
