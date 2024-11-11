63d735b43e87d04765a319c87b2d4ba8ffd46890
Sublimeful <Qiangwu2000@gmail.com>
2024-11-10 20:14:11 -0600

    Done with the general layout of the game screen

--- src/client/Game.tsx
+++ src/client/Game.tsx
@@ -21,10 +21,32 @@ export default function Game() {
         <i className="bi bi-clock"></i>2:31
       </div>
       <div className="grow w-full py-5 flex flex-row gap-2">
-        <div className="flex-1 bg-[#424242] bg-opacity-70 rounded-lg"></div>
+        <div className="flex-1 bg-[#424242] bg-opacity-70 rounded-lg flex flex-col p-4 gap-4">
+          <h1 className="text-2xl flex gap-2 font-semibold">
+            <i className="bi bi-people-fill"></i>Players
+          </h1>
+          <div className="h-12 w-full rounded-lg bg-gradient-to-r from-[#AC1C1C] to-[#2AAAD9] flex flex-row items-center px-3 text-xl">
+            You
+          </div>
+          <div className="h-12 w-full rounded-lg bg-[#5e5e5e] flex flex-row items-center px-3 text-xl">
+            Player 1
+          </div>
+          <div className="h-12 w-full rounded-lg bg-[#5e5e5e] flex flex-row items-center px-3 text-xl">
+            Player 2
+          </div>
+        </div>
         <div className="flex-[3] flex flex-col gap-3">
-          <div className="flex-[3] bg-[#424242] bg-opacity-70 rounded-lg grid place-items-center text-2xl font-semibold">Category: Your Mom</div>
-          <div className="flex-[8] bg-[#424242] bg-opacity-70 rounded-lg"></div>
+          <div className="flex-[3] bg-[#424242] bg-opacity-70 rounded-lg grid place-items-center text-2xl font-semibold">
+            Category: Your Mom
+          </div>
+          <div className="flex-[8] bg-[#424242] bg-opacity-70 rounded-lg p-5">
+            <h1 className="flex gap-2">
+              <span className="font-bold">Player 2:</span>Does she have a gyatt?
+            </h1>
+            <h1 className="flex gap-2 text-[#28dded] ml-3">
+              <span className="font-bold">AI:</span>Yes, she has a gyatt.
+            </h1>
+          </div>
           <form className="flex-1 flex flex-row gap-3">
             <input
               type="text"
