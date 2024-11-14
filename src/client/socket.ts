491bab417296efbbfc9113fc87de7c8eef151929
Sublimeful <Qiangwu2000@gmail.com>
2024-11-14 06:11:41 +0000

    NODE_ENV flag

--- src/client/socket.ts
+++ src/client/socket.ts
@@ -1,4 +1,12 @@
 import { io } from "socket.io-client";
-export const socket = io("https://unravel.sublimeful.org", {
-  autoConnect: false,
-});
+
+console.log("NODE_ENV", process.env["NODE_ENV"]);
+
+export const socket = io(
+  process.env["NODE_ENV"] === "production"
+    ? "https://unravel.sublimeful.org"
+    : "http://localhost:5174",
+  {
+    autoConnect: false,
+  },
+);
