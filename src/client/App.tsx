aded82fa9dcfd086679c01852b82664792f8c5c5
l0st-idi0t <carlnod121@gmail.com>
2024-11-19 13:36:30 -0600

    improved performance by switching from three.js to tailwind for background animations

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,5 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { Canvas } from "@react-three/fiber";
-import { Vector2 } from "three";
 import { useEffect, useState } from "react";
 
 import Background from "./Background";
@@ -77,13 +75,9 @@ export default function App() {
   return (
     <PageContext.Provider value={{ currPage, setPage }}>
       <div className="h-screen w-screen bg-[#040039] flex items-center justify-center overflow-y-clip overflow-x-clip">
-        <Canvas className="w-full h-full blur-3xl animate-pulse">
-          <Background
-            redCircleInitPos={new Vector2(0.15, 0.75)}
-            blueCircleInitPos={new Vector2(0.22, 0.27)}
-            tealCircleInitPos={new Vector2(0.79, 0.69)}
-          />
-        </Canvas>
+        <div className="w-full h-full blur-3xl animate-pulse">
+          <Background />
+        </div>
         {currPage}
       </div>
     </PageContext.Provider>
