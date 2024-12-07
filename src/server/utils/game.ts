55a2ceb45ebb6da85b333dc9ad4b9fbb50c6f258
Sublimeful <Qiangwu2000@gmail.com>
2024-12-06 19:59:09 -0600

    Added awaits to the changing of user elo

--- src/server/utils/game.ts
+++ src/server/utils/game.ts
@@ -23,7 +23,7 @@ export async function gameEnd(roomCode: string, winner: string | null) {
     if (winner) {
       const winnerELO = await getUserELO(winner);
       const winELO = Math.ceil(1000 / (.00039 * Math.pow(winnerELO, 2) + 10));
-      changeUserELO(winner, winELO);
+      await changeUserELO(winner, winELO);
     }
 
     for (const loser of players) {
@@ -34,7 +34,7 @@ export async function gameEnd(roomCode: string, winner: string | null) {
       const loseELO = Math.floor(
         100 - 100 * Math.pow(Math.E, -0.001 * loserELO),
       );
-      changeUserELO(loser, -loseELO);
+      await changeUserELO(loser, -loseELO);
     }
   }
 }
